b2501980e8cac075a7b50cf16387ca22b88b7d9f
Toan Duong <toandt@nexlesoft.com>
2022-11-22 10:45:05 +0700

    v1

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Duong Thanh Toan'
+export const siteTitle = 'Toan App'
 
 export default function Layout({ children, home }) {
   return (
